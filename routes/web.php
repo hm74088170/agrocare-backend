df934995c1ff965dd861b35e9947fb2ed338ce60
Himanshu Mishra <hm74088170@gmail.com>
2026-07-16 17:05:16 +0530

    Add welcome route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
+
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'success' => true,
+        'message' => 'AgroCare Laravel API is running successfully!',
+        'version' => '1.0'
+    ]);
 });
