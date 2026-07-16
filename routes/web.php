b651f2ef516181624f556baf8eec191473eb7cd4
Himanshu Mishra <hm74088170@gmail.com>
2026-07-16 17:37:02 +0530

    test root route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
+
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "AgroCare Root Working";
+    return response()->json([
+        'success' => true,
+        'message' => 'AgroCare Laravel API is running successfully!',
+        'version' => '1.0'
+    ]);
 });
